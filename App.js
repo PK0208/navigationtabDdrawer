448cb5dff30c3e95672c9f0bdf115928c08f8b7f
PraveenN <praveenk@excelerate.com>
2020-11-10 12:36:51 +0530

    navigation 5

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+
+//import { MainStackNavigator } from "./navigation/StackNavigator";
+
+import BottomTabNavigator from "./navigation/TabNavigator";
+import DrawerNavigator from "./navigation/DrawerNavigator";
+
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      {/* <MainStackNavigator /> */}
+      {/* <BottomTabNavigator /> */}
+      <DrawerNavigator />
+    </NavigationContainer>
+  );
+}
+export default App
